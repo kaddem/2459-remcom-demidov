6ee88de518a8252a24666f849f6ae62b1aca92be
Kirill Demidov <kademidov@gmail.com>
2022-01-17 13:24:41 +0300

    фдаптив главной страницы + создание внутренних без уникальных блоков

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,16 @@
 console.log('script work!');
+
+let petsName = ['Gans', 'Ryann', 'Nikki', 'Dusya'];
+
+console.log(  petsName  );
+console.log(  petsName.length  );
+
+
+let person = {
+  name: 'Ivan',
+  age: 23
+}
+
+console.log(person.name);
+
+let summ = 2 + 3 * 3;
